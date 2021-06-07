b538c8347eced948e16b0becc2b6eb116d0b5940
Federica Soriolo <federica.soriolo@stud.tecnicosuperiorekennedy.it>
2021-06-07 17:22:49 +0200

    Caricamento parte frontend

---
+++ PW.RoomTech/PW.RoomTech.Web/Pages/Aule.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PW.RoomTech.Web.Pages
+{
+    public class AuleModel : PageModel
+    {
+        private readonly ILogger<AuleModel> _logger;
+
+        public AuleModel(ILogger<AuleModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ PW.RoomTech/PW.RoomTech/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PW.RoomTech
+{
+    public class Class1
+    {
+    }
+}
